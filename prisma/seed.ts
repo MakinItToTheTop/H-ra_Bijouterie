717b6e8567a580ba42f6c6e1b6b767869ed9dc57
Komurcu Malik <malik.komurcu@etu.univ-nantes.fr>
2026-06-19 16:09:13 +0200

    correction prisma

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,9 +1,14 @@
 import { PrismaClient } from "@/generated/prisma/client";
-import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-const adapter = new PrismaBetterSqlite3({ url: "file:./prisma/dev.db" });
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+  throw new Error("DATABASE_URL is not defined.");
+}
+
 const prisma = new PrismaClient({
-  adapter: adapter,
+  adapter: new PrismaPg({ connectionString }),
 });
 
 const products = [

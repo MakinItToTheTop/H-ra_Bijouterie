76a4c953842a7012ea6805d25548a20f2ee38997
Komurcu Malik <malik.komurcu@etu.univ-nantes.fr>
2026-06-26 15:49:18 +0200

    Fix logout: use redirect: true and disable session refetching

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -6,7 +6,7 @@ import { ToastProvider } from "@/components/Toast";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <SessionProvider>
+    <SessionProvider refetchInterval={0} refetchOnWindowFocus={false}>
       <ToastProvider>
         <CartProvider>{children}</CartProvider>
       </ToastProvider>

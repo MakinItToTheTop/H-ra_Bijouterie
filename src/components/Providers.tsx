c7dc66ec4344f13b043c04cf20ad65e92a0967d4
Komurcu Malik <malik.komurcu@etu.univ-nantes.fr>
2026-07-02 15:38:40 +0200

    ajout d'un curseur customisé

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -3,12 +3,16 @@
 import { SessionProvider } from "next-auth/react";
 import { CartProvider } from "@/context/CartContext";
 import { ToastProvider } from "@/components/Toast";
+import { CustomCursor } from "@/components/CustomCursor";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <SessionProvider>
       <ToastProvider>
-        <CartProvider>{children}</CartProvider>
+        <CartProvider>
+          <CustomCursor />
+          {children}
+        </CartProvider>
       </ToastProvider>
     </SessionProvider>
   );

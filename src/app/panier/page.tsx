adde53b437b79a8d1ff75ba96a3694934f5f2a1f
Malik <zumalikomurcu@gmail.com>
2026-09-05 13:39:18 +0200

    import dupliqué

--- src/app/panier/page.tsx
+++ src/app/panier/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { Suspense, useEffect } from "react";
-import { useEffect } from "react";
+
 import { useSearchParams } from "next/navigation";
 import Link from "next/link";
 import {
